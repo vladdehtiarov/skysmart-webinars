b2a43259d13221c984a4b1fc730428be7cb14b75
vladdehtiarov <vladdegtarov1998@gmail.com>
2021-03-22 02:29:38 +0200

    add form script

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Form
     const form = document.querySelectorAll('.form');
+    const reminder = document.querySelector('.header__reminder');
+    const userSatus = document.querySelector('#user-satus');
+
 
     form.forEach(form => {
         form.addEventListener('click', function(e) {
@@ -12,7 +15,20 @@ window.addEventListener('DOMContentLoaded', function() {
             const stepOne = this.querySelector('.form__inputs_step-1'); 
             const stepTwo = this.querySelector('.form__inputs_step-2'); 
             const formTitle = this.querySelector('.form__title');
+            const signUp = this.querySelector('.sign-up');
 
+            if(signUp) {
+                signUp.addEventListener('click', () => {
+                    userSatus.innerText = 'Вы успешно отправили заявку';
+                    reminder.innerHTML = `
+                        <span class="emoji emoji-mailbox"></span>
+                        <p class="header__reminder-text">
+                            Отправили вам на почту запись вебинара
+                        </p>
+                    `;
+                });
+            }
+            
             if(target.classList.contains('next-form')) {
                 if(formTitle) {
                     formTitle.innerText = 'Шаг 2 из 2';
